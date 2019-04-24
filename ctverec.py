8ffe6a38234403a9ef15b10042e5366e35c65104
Ivcas85 <44992769+Ivcas85@users.noreply.github.com>
2019-04-24 08:41:26 +0200

    na odevzdání

--- ctverec.py
+++ ctverec.py
@@ -8,21 +8,20 @@ class Ctverec(Tvary):
         self.strana = strana
 
     def obvod(self):
-        return self.strana*4
+        return self.strana * 4
 
     def obsah(self):
-        return self.strana**2
+        return self.strana ** 2
 
 class Kruh(Tvary):
     def __init__(self,polomer):
         self.polomer = polomer
 
     def obvod(self):
-        return self.polomer*2*pi
+        return self.polomer * 2 * pi
 
     def obsah(self):
-        return pi*self.polomer**2
-
+        return pi * self.polomer ** 2
 
 class Obdelnik(Tvary):
     def __init__(self,strana_a,strana_b):
@@ -30,18 +29,18 @@ class Obdelnik(Tvary):
         self.strana_b = strana_b
 
     def obvod(self):
-        return 2*(self.strana_a+self.strana_b)
+        return 2 * (self.strana_a + self.strana_b)
 
     def obsah(self):
-        return self.strana_a*self.strana_b
+        return self.strana_a * self.strana_b
 
-ctverec_prvni=Ctverec(9)
-ctverec_druhy=Ctverec(3)
-kruh_prvni=Kruh(9)
-kruh_druhy=Kruh(3)
-obdelnik_prvni=Obdelnik(9,6)
-obdelnik_druhy=Obdelnik(6,3)
+ctverec_prvni = Ctverec(9)
+ctverec_druhy = Ctverec(3)
+kruh_prvni = Kruh(9)
+kruh_druhy = Kruh(3)
+obdelnik_prvni = Obdelnik(9,6)
+obdelnik_druhy = Obdelnik(6,3)
 
-
-
-print(obdelnik_prvni.rozdil_obsahu(kruh_prvni))
+print(ctverec_prvni.rozdil_obsahu(kruh_prvni))
+print(obdelnik_druhy.rozdil_obsahu(kruh_druhy))
+print(kruh_prvni.rozdil_obsahu(obdelnik_druhy))
